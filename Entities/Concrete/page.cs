8d601a00d140d6b09608616695506e90c955030d
Yetiş Şakiroğlu <yetissakiroglu@gmail.com>
2021-04-14 15:48:59 +0300

    Add project files.

---
+++ Entities/Concrete/page.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class page : IEntity
+    {
+        [Key]
+        public int pageId { get; set; }
+        public string img { get; set; }
+        public int modulId { get; set; }
+        public int typeId { get; set; }
+        public int row { get; set; }
+        public string target { get; set; }
+        public bool status { get; set; }
+        public string urlsite { get; set; }
+    }
+}
