da325fe85d5378711ec34e8bd4d94860a78df027
Yetiş Şakiroğlu <yetissakiroglu@gmail.com>
2021-04-19 17:26:15 +0300

    Core and DataAcces başlandı.

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,8 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Core.DataAccess.EntityFramework
+{
+
+}
---
+++ DataAcceess/Concrete/EntityFramework/Contexts/WebDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAcceess.Concrete.EntityFramework.Contexts
+{
+    public class WebDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS; Initial Catalog=afetgiris; Integrated Security=True; Persist Security Info=False");
+            base.OnConfiguring(optionsBuilder);
+
+        }
+    }
+
+
+}
